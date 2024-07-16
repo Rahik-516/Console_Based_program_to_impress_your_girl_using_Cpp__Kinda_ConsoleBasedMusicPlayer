48bc374f2ec9e29c587c62881aef4965dca130cf
I. S. M  Rahik <143213420+Rahik-516@users.noreply.github.com>
2024-07-16 08:23:39 +0600

    Add files via upload

---
+++ Agun build.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<windows.h>
+#include<mmsystem.h>
+#include<stdio.h>
+#include<string>
+#include<chrono>
+#include<thread>
+
+using namespace std;
+
+void start_page()
+{
+
+    cout<< "________________________________________________________________"<<endl;
+    cout<<" |--IS the person that you like/love a CSE STUDENT??? --------->|"<<endl;
+    cout<<" |--If yes, then try to impress her with these kind of programs.|"<<endl;
+    cout<<" |                     Let's get started!                       |"<<endl;
+    cout<<" |------------------------------------------------------------- |"<<endl;
+    cout<<" |--------------------------------------------------------------|"<<endl;
+    Sleep(3000);
+    system("cls");
+    cout<<" |  ----------------------------------------------------------- |"<<endl;
+    cout<<" |  ----------------------------------------------------------- |"<<endl;
+    cout<<" |  ------Tomader bhabir pochonder gaan select korsi ami------- |"<<endl;
+    cout<<" |  ---Tomra tomader bhalobashar manusher pochonder gaan------- |"<<endl;
+    cout<<" |                      select korte paro                       |"<<endl;
+    cout<<" |  ----------------------------------------------------------- |"<<endl;
+    cout<<" |--------------------------------------------------------------|"<<endl;
+    Sleep(3000);
+    system("cls");
+    cout<<" | Priyotoma, Sudhu tomar jonnei baniyechi ei program ta------->|"<<endl;
+    cout<<" |                                                              |"<<endl;
+    cout<<" | Asha kori tomar bhalo lagbe!.....                            |"<<endl;
+    cout<<" |  ----------------------------------------------------------- |"<<endl;
+    cout<<" |______________________________________________________________| "<<endl;
+    Sleep(3000);
+    system("cls");
+
+}
+
+
+
+int main()
+{
+    system("Color FC");
+    start_page();
+    char ch[100];
+    cout<<""<<endl;
+    cout<<"                Assalamu Alaikum        "<<endl;
+    cout<<"     ______________________________________"<<endl;
+
+    cout<<"    Shahanaz Parvin Chaity, eita tomar jonne !"<<endl;
+    Sleep(3000);
+
+        system("cls");
+        system("Color 3F");
+        PlaySound(TEXT("Onek kore pabo.wav"),NULL,SND_ASYNC);
+        for(int i=0; i<15; i++)
+        {
+            cout<<"<3  <3  <3  <3  <3  <3  <3  <3  <3  <3  <3  <3  <3  ";
+            Sleep(1400);
+
+        }
+        for(int i=0; i<2; i++)
+        {
+            cout<<"\n\n\n\t\t\t        Onek kore pabo, tomar  "<<endl;
+             Sleep(3000);
+            cout<<"\t\t\t\t                Onek beshi hobo <3"<<endl;
+             Sleep(3000);
+        }
+
+
+        for(int i=0; i<2; i++)
+        {
+            cout<<"\n\n\n\t\t\t    Haat ta dhore, poth ta bhule    <3"<<endl;
+             Sleep(2600);
+            cout<<"\t\t\t\t               moner kotha kobo"<<endl;
+             Sleep(2600);
+        }
+        system("cls");
+        Sleep(2000);
+
+
+        cout<<"\n\n\n\t\t\t    Amar feeling tomar proti ei gaan diyei nahoi bujhalam~ <3"<<endl;
+        Sleep(2500);
+        cout<<"\t\t\t\tTumi sathe thakle prochondo rokomer bhalo lage amar"<<endl;
+        Sleep(2500);
+        cout<<"\t\t\t\t\t   Trust kore dekhte paro,"<<endl;
+        Sleep(2500);
+        cout<<"\t\t\t\t\t  Kosto pete dibona KOKHONO!"<<endl;
+        Sleep(3000);
+        system("cls");
+        system("cls");
+        Sleep(3000);
+
+
+
+    return 0;
+
+}
+
+
+
